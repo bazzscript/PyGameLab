ccf0d9d3468c9b6555a36275b2498ba27fc38bbc
emexbazz <bmexit@gmail.com>
2021-05-15 01:04:52 +0100

    add a few games

---
+++ madlibs.py/maina.py
@@ -0,0 +1,9 @@
+# nothing fun here, just relearning strings concatenation
+
+name = input("Your Name: ")
+passion1 = input("What is the one thing you enjoy doing? ")
+passion2 = input("What is that one thing you are most good at? ")
+superhero = input("Your Favourite Super Hero? ")
+madlibs = f"My name is {name}, i love the idea behind {passion1} and i also love {passion2} whenever i get to do both at the same time , i feel like {superhero}!"
+
+print(madlibs)
